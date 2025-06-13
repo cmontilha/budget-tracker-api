975507795679aca1ccd3046f5f6cbfea3ac911dc
cmontilha <caiomontilha.cm@gmail.com>
2025-06-13 10:12:51 -0400

    Initial commit with Dropwizard project structure

---
+++ src/main/java/com/cmontilha/budget/budget-tracker-apiConfiguration.java
@@ -0,0 +1,10 @@
+package com.cmontilha.budget;
+
+import io.dropwizard.Configuration;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.*;
+import javax.validation.constraints.*;
+
+public class budget-tracker-apiConfiguration extends Configuration {
+    // TODO: implement service configuration
+}
