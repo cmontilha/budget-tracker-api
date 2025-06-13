d10021b83632beca4481dff5550721e3e1233656
cmontilha <caiomontilha.cm@gmail.com>
2025-06-13 11:13:47 -0400

    Add User.java entity class with JPA annotations for persistence

---
+++ src/main/java/com/cmontilha/budget/core/User.java
@@ -0,0 +1,21 @@
+package com.cmontilha.budget.core;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    // Getters e Setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+}
